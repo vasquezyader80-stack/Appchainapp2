c95967f2a538f17c92481d911127ac08899da9b4
vasquezyader80-stack <vasquezyader80@gmail.com>
2026-03-01 14:47:46 -0600

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,20 +1,14 @@
 const Inventory = {
-    // Estos productos aparecerán automáticamente
+    // Estos productos son los que aparecen sin que tú hagas nada
     defaultProducts: [
-        { id: 1, name: "YaraVila Amidas (Fertilizante)", price: 1250, img: "🌱" },
-        { id: 2, name: "Semilla Maíz Pioneer (Bolsa)", price: 3400, img: "🌽" },
+        { id: 1, name: "YaraVila Amidas", price: 1250, img: "🌱" },
+        { id: 2, name: "Semilla Maíz Pioneer", price: 3400, img: "🌽" },
         { id: 3, name: "Bomba de Riego Solar", price: 8900, img: "☀️" },
         { id: 4, name: "Dron de Fumigación T40", price: 15500, img: "🛸" }
     ],
     getAll() {
-        // Esta línea une los productos de fábrica con los que tú registres
         const custom = JSON.parse(localStorage.getItem('agro_market')) || [];
         return [...this.defaultProducts, ...custom];
-    },
-    add(name, price) {
-        const products = JSON.parse(localStorage.getItem('agro_market')) || [];
-        products.push({ id: Date.now(), name, price: parseFloat(price), img: "📦" });
-        localStorage.setItem('agro_market', JSON.stringify(products));
     },
     remove(id) {
         const products = JSON.parse(localStorage.getItem('agro_market')) || [];
